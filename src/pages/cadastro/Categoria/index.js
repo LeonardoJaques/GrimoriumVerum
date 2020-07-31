ece4444371f36b48d008badc01499e30d1e5fef9
LeonardoJaques <jaques.projetos@outlook.com>
2020-07-31 01:26:39 -0300

    Fix Typing

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-React Alura Immersion Weekimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
