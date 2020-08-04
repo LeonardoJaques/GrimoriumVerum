107e42c4158593957fd1ebed094d0981a8c7225f
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-04 15:39:21 -0300

    step 3 of home, changed PageDefault and inserting links and videos in database

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
-// import Menu from '../../components/Menu'
-// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-
 import categoriasRepository from '../../repositories/categorias';
 import PageDefault from '../../components/PageDefault';
 
@@ -20,27 +17,34 @@ function Home() {
         console.log(err.message);
       });
   }, []);
-  // http://localhost:3001/categorias?_embed=videos
 
   return (
-    <PageDefault>
-      {/* {JSON.stringify(initialData)} */}
-
+    <PageDefault paddingAll={0}>
       {initialData.length === 0 && (<div>Loading...</div>)}
-      {initialData.length > 1 && (
-        <>
-          {' '}
-          <BannerMain
-            videoTitle={initialData[0].videos[0].title}
-            url={initialData[0].videos[0].url}
-            videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-          />
+      {initialData.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={initialData[0].videos[0].title}
+                url={initialData[0].videos[0].url}
+                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+              />
 
-          <Carousel ignoreFirstVideo category={initialData[0]} />
-          <Carousel category={initialData[1]} />
-          
-        </>
-      )}
+              <Carousel
+                ignoreFirstVideo
+                category={initialData[0]}
+              />
+            </div>
+          );
+        }
+        return (
+          <Carousel
+            ket={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
     </PageDefault>
   );
