0c45c0a53a1de45521b0fbcafa189585a5fe9086
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-04 12:22:18 -0300

    step 3 in home and fix the carousel

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -40,15 +40,14 @@ export const SliderItem = styled.li`
 
 const Slider = ({ children }) => (
   <Container>
-    <SlickSlider
-      {...{
-        dots: false,
-        infinite: false,
-        speed: 300,
-        centerMode: false,
-        variableWidth: true,
-        adaptiveHeight: true,
-      }}
+    <SlickSlider {...{
+      dots: false,
+      infinite: false,
+      speed: 300,
+      centerMode: false,
+      variableWidth: true,
+      adaptiveHeight: true,
+    }}
     >
       {children}
     </SlickSlider>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+import React, { useEffect, useState } from 'react';
+// import Menu from '../../components/Menu'
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carrousel';
-import Footer from '../../components/Footer';
+import Carousel from '../../components/Carousel';
+
 import categoriasRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
+  const [initialData, setData] = useState([]);
+
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
         console.log(categoriasComVideos);
+        setData(categoriasComVideos);
       })
       .catch((err) => {
         console.log(err.message);
@@ -19,29 +23,26 @@ function Home() {
   // http://localhost:3001/categorias?_embed=videos
 
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault>
+      {/* {JSON.stringify(initialData)} */}
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-      />
+      {initialData.length === 0 && (<div>Loading...</div>)}
+      {initialData.length > 1 && (
+        <>
+          {' '}
+          <BannerMain
+            videoTitle={initialData[0].videos[0].title}
+            url={initialData[0].videos[0].url}
+            videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+          />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+          <Carousel ignoreFirstVideo category={initialData[0]} />
+          <Carousel category={initialData[1]} />
+          
+        </>
+      )}
 
-      <Carousel category={dadosIniciais.categorias[1]} />
-
-      <Carousel category={dadosIniciais.categorias[2]} />
-
-      <Carousel category={dadosIniciais.categorias[3]} />
-
-      <Carousel category={dadosIniciais.categorias[4]} />
-
-      <Carousel category={dadosIniciais.categorias[5]} />
-
-      <Footer />
-    </div>
+    </PageDefault>
   );
 }
 export default Home;
